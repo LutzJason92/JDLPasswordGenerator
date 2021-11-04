dda8e8527a20c571f00becf6acb943b41a8edebc
Jason <LutzJason92@gmail.com>
2021-11-04 17:22:03 -0600

    Update script.js
    outlined general functions for pw generator

--- Assets/script.js
+++ Assets/script.js
@@ -1,27 +1,48 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
+// }
+// //create generatePassword function
+
+// //prompt user for legnth
+// //error handling 
+
+// // errror handeling - did they gen a valid number
+
+// //prompt user for legnth
+// //promt to confirm all characterslection
+
+// //did user select at least 1 option
+
+
+// //prompt use to 
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+function getRandomLower(){
+  return String.fromCharCode(Math.floor(Math.random() * 26)+97);
+
 }
-//create generatePassword function
+function getRandomUpper(){
+  return String.fromCharCode(Math.floor(Math.random() * 26)+65);
 
-//prompt user for legnth
-//error handling 
+}
 
-// errror handeling - did they gen a valid number
+function getRandomNumber(){
+  return String.fromCharCode(Math.floor(Math.random() * 10)+48);
 
-//prompt user for legnth
-//promt to confirm all characterslection
+}
 
-//did user select at least 1 option
+function getRandomSymbol(){
+  const symbols = "!'#$%&()*+-.<>/";
+  return symbols[Math.floor(Math.random() * symbols.length)];
 
-
-//prompt use to 
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+}
+console.log(getRandomSymbol())
